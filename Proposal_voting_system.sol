e3659feb75fed60783b21d08eb968f1f0e3dc90f
Alex <alexandermcneill53@outlook.com>
2026-02-25 22:59:14 +1100

    2. Update Proposal_voting_system.sol
    Update of Proposal voting system to include a counter to keep track of proposals, as well as a function to create proposals for us.

--- Proposal_voting_system.sol
+++ Proposal_voting_system.sol
@@ -7,6 +7,9 @@ pragma solidity ^0.8.18;
 
 contract ProposalContract {
 
+    // Counter to track of the IDs
+    uint256 private counter;
+
     struct Proposal {
         
         // Title of proposal
@@ -19,17 +22,25 @@ contract ProposalContract {
         uint256 reject;
         // Number of pass votes
         uint256 pass;
-        // Total vote to end
+        // Total vote to end when this limit is reached
         uint256 total_vote_to_end;
-        // Current state
+        // Current state whether it will pass or fail
         bool current_state;
-        // status - active
+        // status whether active and others can vote to contract
         bool is_active;
 
     }
 
     // Recording of previous proposals
     mapping(uint256 => Proposal) proposal_history;
+
+    // Function to create proposals
+    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+
+        proposal_history[counter] = Proposal(_title, _description, 0, 0, 0, _total_vote_to_end, false, true);
+
+    }
 
 
 }
